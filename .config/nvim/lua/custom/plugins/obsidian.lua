9081075d82ebf83714d6f97bdec2603c4040c6ae
Luis Mata <luismata@luishmata.com>
2024-05-25 17:41:01 -0600

    update nvim obsidian.lua

--- .config/nvim/lua/custom/plugins/obsidian.lua
+++ .config/nvim/lua/custom/plugins/obsidian.lua
@@ -1,124 +1,125 @@
 return {
-  {
-    'epwalsh/obsidian.nvim',
-    version = '*',
-    lazy = true,
-    event = {
-      'BufReadPre ' .. vim.fn.expand '~' .. '/Notes/**.md',
-      'BufNewFile path/to/my-vault/**.md',
-    },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    keys = {
-      { '<leader>nd', '<cmd>ObsidianToday<cr>', desc = "Open Todays's note" },
-      { '<leader>no', '<cmd>ObsidianOpen<cr>', desc = 'Open in Obsidian' },
-      { '<leader>np', '<cmd>ObsidianPasteImg<cr>', desc = 'Paste image(Obsidian)' },
-      { '<leader>nn', '<cmd>ObsidianQuickSwitch<cr>', desc = 'Quick Switcher(Obsidian)' },
-      { '<leader>nt', '<cmd>ObsidianTemplate<cr>', desc = 'Insert Template' },
-      { '<leader>nb', '<cmd>ObsidianTemplate Basic<cr>', desc = 'Insert Basic Template' },
-    },
-    opts = {
-      workspaces = {
-        {
-          name = 'n',
-          path = '~/Notes',
-        },
-      },
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		event = {
+			"BufReadPre " .. vim.fn.expand("~") .. "/Notes/**.md",
+			"BufNewFile path/to/my-vault/**.md",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>nd", "<cmd>ObsidianToday<cr>", desc = "Open Todays's note" },
+			{ "<leader>no", "<cmd>ObsidianOpen<cr>", desc = "Open in Obsidian" },
+			{ "<leader>np", "<cmd>ObsidianPasteImg<cr>", desc = "Paste image(Obsidian)" },
+			{ "<leader>nn", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switcher(Obsidian)" },
+			{ "<leader>nt", "<cmd>ObsidianTemplate<cr>", desc = "Insert Template" },
+			{ "<leader>nb", "<cmd>ObsidianTemplate Basic<cr>", desc = "Insert Basic Template" },
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "n",
+					path = "~/Notes",
+				},
+			},
 
-      log_level = vim.log.levels.INFO,
+			log_level = vim.log.levels.INFO,
 
-      daily_notes = {
-        folder = 'inbox/',
-        date_format = '%Y-%m-%d',
-      },
+			daily_notes = {
+				folder = "inbox/",
+				date_format = "%Y-%m-%d",
+			},
 
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-      },
+			completion = {
+				nvim_cmp = true,
+				min_chars = 2,
+			},
 
-      preferred_link_style = 'markdown',
+			preferred_link_style = "markdown",
 
-      note_frontmatter_func = function(note)
-        -- Add the title of the note as an alias.
-        if note.title then
-          note:add_alias(note.title)
-        end
+			note_frontmatter_func = function(note)
+				-- Add the title of the note as an alias.
+				if note.title then
+					note:add_alias(note.title)
+				end
 
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+				local out = { id = note.id, aliases = note.aliases, tags = note.tags }
 
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
+				-- `note.metadata` contains any manually added fields in the frontmatter.
+				-- So here we just make sure those fields are kept in the frontmatter.
+				if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+					for k, v in pairs(note.metadata) do
+						out[k] = v
+					end
+				end
 
-        return out
-      end,
+				return out
+			end,
 
-      notes_subdir = 'inbox',
-      image_name_func = function()
-        -- Prefix image names with timestamp.
-        return string.format('%s-', os.time())
-      end,
-      new_notes_location = 'notes_subdir',
+			notes_subdir = "inbox",
 
-      note_id_func = function(title)
-        local suffix = ''
-        if title ~= nil then
-          suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
-        else
-          -- If title is nil, just add 4 random uppercase letters to the suffix.
-          for _ = 1, 4 do
-            suffix = suffix .. string.char(math.random(65, 90))
-          end
-        end
-        return tostring(os.time()) .. '-' .. suffix
-      end,
-      templates = {
-        subdir = 'Templates',
-        date_format = '%Y-%m-%d',
-        time_format = '%H:%M',
-        -- A map for custom variables, the key should be the variable and the value a function
-        substitutions = {},
-      },
+			image_name_func = function()
+				-- Prefix image names with timestamp.
+				return string.format("%s-", os.time())
+			end,
+			new_notes_location = "notes_subdir",
 
-      ui = {
-        enable = true, -- set to false to disable all additional syntax features
-        update_debounce = 200, -- update delay after a text change (in milliseconds)
-        -- Define how various check-boxes are displayed
-        checkboxes = {
-          [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
-          ['x'] = { char = '', hl_group = 'ObsidianDone' },
-          ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
-          ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
-        },
-        -- Use bullet marks for non-checkbox lists.
-        bullets = { char = '•', hl_group = 'ObsidianBullet' },
-        external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
-        -- Replace the above with this if you don't have a patched font:
-        -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-        reference_text = { hl_group = 'ObsidianRefText' },
-        highlight_text = { hl_group = 'ObsidianHighlightText' },
-        tags = { hl_group = 'ObsidianTag' },
-        block_ids = { hl_group = 'ObsidianBlockID' },
-        hl_groups = {
-          -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-          ObsidianTodo = { bold = true, fg = '#f78c6c' },
-          ObsidianDone = { bold = true, fg = '#89ddff' },
-          ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
-          ObsidianTilde = { bold = true, fg = '#ff5370' },
-          ObsidianBullet = { bold = true, fg = '#89ddff' },
-          ObsidianRefText = { underline = true, fg = '#c792ea' },
-          ObsidianExtLinkIcon = { fg = '#c792ea' },
-          ObsidianTag = { italic = true, fg = '#89ddff' },
-          ObsidianBlockID = { italic = true, fg = '#89ddff' },
-          ObsidianHighlightText = { bg = '#75662e' },
-        },
-      },
-    },
-  },
+			note_id_func = function(title)
+				local suffix = ""
+				if title ~= nil then
+					suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+				else
+					-- If title is nil, just add 4 random uppercase letters to the suffix.
+					for _ = 1, 4 do
+						suffix = suffix .. string.char(math.random(65, 90))
+					end
+				end
+				return tostring(os.time()) .. "-" .. suffix
+			end,
+			templates = {
+				subdir = "Templates",
+				date_format = "%Y-%m-%d",
+				time_format = "%H:%M",
+				-- A map for custom variables, the key should be the variable and the value a function
+				substitutions = {},
+			},
+
+			ui = {
+				enable = true, -- set to false to disable all additional syntax features
+				update_debounce = 200, -- update delay after a text change (in milliseconds)
+				-- Define how various check-boxes are displayed
+				checkboxes = {
+					[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+					["x"] = { char = "", hl_group = "ObsidianDone" },
+					[">"] = { char = "", hl_group = "ObsidianRightArrow" },
+					["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+				},
+				-- Use bullet marks for non-checkbox lists.
+				bullets = { char = "•", hl_group = "ObsidianBullet" },
+				external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+				-- Replace the above with this if you don't have a patched font:
+				-- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+				reference_text = { hl_group = "ObsidianRefText" },
+				highlight_text = { hl_group = "ObsidianHighlightText" },
+				tags = { hl_group = "ObsidianTag" },
+				block_ids = { hl_group = "ObsidianBlockID" },
+				hl_groups = {
+					-- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+					ObsidianTodo = { bold = true, fg = "#f78c6c" },
+					ObsidianDone = { bold = true, fg = "#89ddff" },
+					ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+					ObsidianTilde = { bold = true, fg = "#ff5370" },
+					ObsidianBullet = { bold = true, fg = "#89ddff" },
+					ObsidianRefText = { underline = true, fg = "#c792ea" },
+					ObsidianExtLinkIcon = { fg = "#c792ea" },
+					ObsidianTag = { italic = true, fg = "#89ddff" },
+					ObsidianBlockID = { italic = true, fg = "#89ddff" },
+					ObsidianHighlightText = { bg = "#75662e" },
+				},
+			},
+		},
+	},
 }
