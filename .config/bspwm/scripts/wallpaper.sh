d0bdfdafb22750d731f6bea04e9d304927c80aaa
Luis Mata <luismata@luishmata.com>
2024-05-27 10:26:52 -0600

    move linux-specific files to its own branch

--- .config/bspwm/scripts/wallpaper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nitrogen --random ~/.config/bspwm/wallpapers/ --set-zoom-fill --head=0
-nitrogen --random ~/.config/bspwm/wallpapers/ --set-zoom-fill --head=1
--- .config/polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-polybar-msg cmd quit
-
-polybar DP --config=$HOME/.config/polybar/config.ini &
-polybar HDMI --config=$HOME/.config/polybar/config.ini &
-disown
